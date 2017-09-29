3d51c3ee1499024a2dcf30ab011e263502847320
shgwwwr <generativepassion@gmail.com>
2017-09-29 19:20:06 +0900

    add - Vue - 登録した内容をリストで表示できるようにした

--- src/components/HomeList.vue
+++ src/components/HomeList.vue
@@ -4,20 +4,30 @@
     .container
       .row
         h1 Welcome to Ad-Hunter!!!
-        tabel.table.table-hover.table-bordered
+        tabel.table.table-hover.table-responsive
           thead
             th No
             th URL
-            th Created_At
+            th Start
+            th Stop
+            th Updated At
             th Ready
+            th Save
             th Edit
           tbody
-            tr
+            tr(v-for='(hunted, index) in htdList')
+              td {{hunted.id}}
+              td {{hunted.url}}
               td
+                tr {{hunted.settings.start | formatDateTime}}
+                tr: input(type='datetime-local' v-model='hunted.settings.start')
               td
-              td
-              td: button.btn.btn-success(type='button' v-on:click='start') start
-              td
+                tr {{hunted.settings.stop | formatDateTime}}
+                tr: input(type='datetime-local' v-model='hunted.settings.stop')
+              td {{hunted.updatedAt | formatDateTime}}
+              td: button.btn.btn-success(type='button' v-on:click='ready') ready
+              td: button.btn.btn-warning(type='button' v-on:click='save(hunted)') save
+              td: button.btn.btn-info(type='button' v-on:click='edit(hunted.id)') edit
 
         .col.text-right
           button.btn.btn-primary(type='button' v-on:click='goCreatePage') Create New
@@ -26,42 +36,74 @@
 
 <script>
   /* eslint-disable */
-  import { ipcRenderer } from 'electron';
   import router from '../router';
+  import { HuntedObject } from '../model/Hunted';
+  import moment from 'moment';
 
   export default {
     name: 'homeList',
 
     data() {
       return {
+        htdList: [],
       };
     },
 
     mounted() {
-
+      this.init();
     },
 
     methods: {
-      start() {
-        window.localStorage.setItem('now', new Date());
-        ipcRenderer.send('crawlExec', 'From Hello');
-        ipcRenderer.on('fromParent', (event, target) => {
-          console.log(target, event);
+      init() {
+        this.getHtdList();
+        this.createFlagList();
+      },
+
+      getHtdList() {
+        this.htdList = HuntedObject.getAll();
+      },
+
+      createFlagList() {
+        this.htdList.forEach(() => {
+          this.startEditFlagList.push(false);
+          this.stopEditFlagList.push(false);
         });
       },
+
       goCreatePage() {
-        router.push('/set-hunter');
-      }
+        router.push({ path: 'set-hunter' });
+      },
+
+      save(htd) {
+        console.log(htd);
+        HuntedObject.save(htd);
+      },
+
+      edit(id) {
+        router.push({ path: 'set-hunter', query: { id: id }});
+      },
     },
 
-    computed: {},
+    computed: {
+    },
 
-    filters: {},
+    filters: {
+      formatDateTime(value) {
+        if (value === '') return '';
+        return moment(value).format('YYYY/MM/DD HH:mm');
+      },
+    },
 
-    watch: {},
+    watch: {
+    },
   };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .table {
+    max-width: none;
+    table-layout: fixed;
+    word-wrap: break-word;
+  }
 </style>
