98e0c40e188e7422141c0e7b2895b2f0372b2c1d
Hagi013 <generativepassion@gmail.com>
2017-11-12 00:14:41 +0900

    refactor - VUE - LocalStorageからのデータ取得の処理をHuntedとUserAgentとで共通化

--- src/lib/repository/HuntedRepository.js
+++ src/lib/repository/HuntedRepository.js
@@ -1,51 +1,10 @@
-import { getItemFromStorage, upsertItemToStorage } from '../localstorage/localstorage';
-import { notArrayCheck } from '../utils/CheckUtils';
+// import { getItemFromStorage, upsertItemToStorage } from '../localstorage/localstorage';
+// import { notArrayCheck } from '../utils/CheckUtils';
+import BaseRepository from './BaseRepository';
 
-export default class HuntedRepository {
+export default class HuntedRepository extends BaseRepository {
 
-  static findAll() {
-    return getItemFromStorage('hunted');
-  }
-
-  static findById(id) {
-    return this.findAll().filter(h => h.id === id)[0];
-  }
-
-  static upsert(hunted) {
-    const all = this.findAll();
-    console.log(all);
-
-    // 初回新規登録
-    if (notArrayCheck(all)) {
-      this.upsertToStorage([hunted]);
-      return this.findById(hunted.id);
-    }
-
-    const existing = all.filter(h => h.id === hunted.id).length > 0;
-
-    // 更新
-    if (existing) {
-      this.upsertToStorage(all.map((h) => {
-        if (h.id === hunted.id) return hunted;
-        return h;
-      }));
-      return this.findById(hunted.id);
-    }
-
-    // 新規登録
-    all.push(hunted);
-    this.upsertToStorage(all);
-    return this.findById(hunted.id);
-  }
-
-  static upsertToStorage(huntedList) {
-    upsertItemToStorage('hunted', huntedList);
-  }
-
-  static remove(id) {
-    this.findAll().forEach((h) => {
-      console.log(id, h.id, id === h.id);
-    });
-    this.upsertToStorage(this.findAll().filter(h => h.id !== id));
+  static repoName() {
+    return 'hunted';
   }
 }
